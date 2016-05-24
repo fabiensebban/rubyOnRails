08b1614c77d95cbff4ca13decae84918e41dd411
Fabian Sebban <fa.sebban@gmail.com>
2016-05-24 18:02:52 +0200

    Ajout de la validation dune image pour un pokemon

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,7 +1,21 @@
 #app/models/pokemon.rb
 
 class Pokemon < ActiveRecord::Base 
+
 	belongs_to :type
 	has_many :pokemon_moves
   	has_many :moves, through: :pokemon_moves
+
+  	has_attached_file :avatar, styles: {
+  		medium: '300x300>',
+  		thumb: '100x100>'
+  	}
+
+  	validates_attachment_content_type :avatar, content_type: /\Aimage/
+  	validates_attachment_file_name :avatar, matches: [/png\Z/, /jpe?g\Z/, /gif\Z/]
+  	validates_with AttachmentSizeValidator,
+  		attributes: :avatar,
+  		less_than: 1.megabytes
+  	do_not_validate_attachment_file_type :avatar
+
 end
