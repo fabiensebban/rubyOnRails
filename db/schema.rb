ef664750eb6a40d3c9466840b2f11e53609832cb
Fabian Sebban <fa.sebban@gmail.com>
2016-05-24 18:38:35 +0200

    ajout du champs username dans la table user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160524154819) do
+ActiveRecord::Schema.define(version: 20160524163636) do
 
   create_table "moves", force: :cascade do |t|
     t.string   "name"
@@ -67,9 +67,11 @@ ActiveRecord::Schema.define(version: 20160524154819) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
