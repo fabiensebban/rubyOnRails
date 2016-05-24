bb4017b7764175900b82748e4476c472ded49353
Fabian Sebban <fa.sebban@gmail.com>
2016-05-24 18:55:44 +0200

    ajout de validation sur d autre champs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :configuration_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configuration_permitted_parameters
+    devise_parameters_sanitizer.for(:sign_up) << :username
+    devise_parameters_sanitizer.for(:account_update) << :username
+  end
 end
