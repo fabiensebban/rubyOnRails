e67ee1d549ab06b4e789222cf1aed607cb5db26f
Fabian Sebban <fa.sebban@gmail.com>
2016-05-25 18:46:54 +0200

    ajout de permissions sur la vue et le controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
+#Controller de base a tous les controllers
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   before_filter :configuration_permitted_parameters, if: :devise_controller?
+
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = exception.message
+    if request.env['HTTP_REFERER'].present?
+      redirect_to :back
+    else
+      redirect_to root_url
+    end
+  end
 
   protected
 
