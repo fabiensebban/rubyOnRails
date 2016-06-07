7570d13314a557a54f5622411c56f63379c4223f
Fabian Sebban <fa.sebban@gmail.com>
2016-06-07 10:40:46 +0200

    Ajout de la fonctionnalité frindly id sur les pokemons

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,21 +1,34 @@
 #app/models/pokemon.rb
 
 class Pokemon < ActiveRecord::Base 
+	extend FriendlyId
 
-	belongs_to :type
-	has_many :pokemon_moves
-  	has_many :moves, through: :pokemon_moves
+  belongs_to :type
+  has_many :pokemon_moves
+  has_many :moves, through: :pokemon_moves
 
-  	has_attached_file :avatar, styles: {
-  		medium: '300x300>',
-  		thumb: '100x100>'
-  	}
+  has_attached_file :avatar, styles: {
+    medium: '300x300>',
+    thumb: '100x100>'
+  }
 
-  	validates_attachment_content_type :avatar, content_type: /\Aimage/
-  	validates_attachment_file_name :avatar, matches: [/png\Z/, /jpe?g\Z/, /gif\Z/]
-  	validates_with AttachmentSizeValidator,
-  		attributes: :avatar,
-  		less_than: 1.megabytes
-  	do_not_validate_attachment_file_type :avatar
+  validates_attachment_content_type :avatar, content_type: /\Aimage/
+  validates_attachment_file_name :avatar, matches: [/png\Z/, /jpe?g\Z/, /gif\Z/]
+  validates_with AttachmentSizeValidator,
+    attributes: :avatar,
+    less_than: 1.megabytes
+  do_not_validate_attachment_file_type :avatar
+
+  # validates :username, presence: true, uniqueness: true
+
+  friendly_id :slug_candidates, use: [:slugged, :finders]
+
+  def slug_candidates
+    [ :name, [:id, :name] ]
+  end
+
+  def should_generate_new_friendly_id?
+    name_changed? || super
+  end
 
 end
